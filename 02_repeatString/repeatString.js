edeacf1636ec31263b6832488b50a60aeea7aaae
N3mEee <n3mebusiness@gmail.com>
2022-10-04 23:34:09 +0200

    Add solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let result = '';
+    if (num < 0) {
+        result = 'ERROR'
+        return result
+    }
 
+    for (i = 1; i <= num; i++) {
+        result += string;
+    }
+    return result
 };
-
 // Do not edit below this line
 module.exports = repeatString;
