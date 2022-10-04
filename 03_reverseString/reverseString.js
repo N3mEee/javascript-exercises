18b61ebeaab2267936e152054427752992dbf4e2
N3mEee <n3mebusiness@gmail.com>
2022-10-04 23:41:00 +0200

    Add solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reverse = ''
+    for (i = input.length; i >= 0; i--){
+        reverse += input.charAt(i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
