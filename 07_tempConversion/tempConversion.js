af566c8475a3c8c31e126dce37b85e9a9312c44f
N3mEee <n3mebusiness@gmail.com>
2022-10-05 14:52:49 +0200

    Add solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let c = (num-32)*5/9
+  c = c.toFixed(1)
+  c = Number(c)
+  return c
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let f = (num*9/5)+32
+  f = f.toFixed(1)
+  f = Number(f)
+  return f
 };
 
 // Do not edit below this line
