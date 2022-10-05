410911152a245b801316ef0a7c0e47b016ee525e
N3mEee <n3mebusiness@gmail.com>
2022-10-05 11:42:44 +0200

    Add solution (works but is not the best)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, argument1, argument2 = test, argument3 = test, argument4 = test) {
+    for (let i = array.length -1; i >= 0; i--) {
+        if (argument1 === array[i] || argument2 === array[i] || argument3 === array[i] || argument4 === array[i]) {
+            array.splice(i, 1)
+        }
+    }
+    return array;
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
