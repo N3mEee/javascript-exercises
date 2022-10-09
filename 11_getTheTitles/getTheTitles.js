c014f280059a116dd6033567f7ca4c6054b31763
N3mEee <n3mebusiness@gmail.com>
2022-10-09 16:01:36 +0200

    Add solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let booksArray = new Array();
+    books.forEach(item => {
+        booksArray.push(item['title'])
+    });
+    return booksArray
 };
 
 // Do not edit below this line
