4c05cbe8f29c98b4f6bc8b98e113b693de3a57b2
N3mEee <n3mebusiness@gmail.com>
2022-10-09 17:05:59 +0200

    Add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arrayPeople) {
+    let currentYear = new Date().getFullYear()
+    let x = arrayPeople.sort((a, b) => {
+        let last = 0;
+        let next = 0;
+        if (a.yearOfDeath === undefined || b.yearOfDeath === undefined) {
+            last = currentYear - a.yearOfBirth;
+            next = currentYear - b.yearOfBirth;
+        } else {
+            last = a.yearOfDeath - a.yearOfBirth;
+            next = b.yearOfDeath - b.yearOfBirth;
+        }
+        return last > next ? -1 : 1;
+    })
+    return x[0]
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+//reduce
