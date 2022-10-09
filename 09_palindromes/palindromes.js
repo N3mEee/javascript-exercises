6bd764e3c37d576d10c595fd27ee44dabddc0b7d
N3mEee <n3mebusiness@gmail.com>
2022-10-09 15:10:58 +0200

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let array = string.split('')
+    array = array.filter((item) => {
+        return (item === ` ` || item === '.' || item === '!' || item === ',') ?false :true;
+    })
+    array = array.every((element, index) => {
+        return element === array.reverse()[index];
+    })
+    return (array) ?true :false;
+    }
 
 // Do not edit below this line
 module.exports = palindromes;
